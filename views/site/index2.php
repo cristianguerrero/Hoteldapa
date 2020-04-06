4b6b7b40b742ae53ed0fc7fb4759330473f6c54f
Cristian Guerrero <c.a.g.9650@hotmail.com>
2020-04-06 11:59:56 -0500

    mas cambios segundo estilo
    se agregaron secciones y se hicieron cambios de textos.

--- views/site/index2.php
+++ views/site/index2.php
@@ -11,63 +11,60 @@ $this->title = 'Hotel Dapa';
 	<div id="demo-1" data-zs-src='["pagina/images/4.jpg", "pagina/images/2.jpg", "pagina/images/1.jpg","pagina/images/3.jpg"]' data-zs-overlay="dots">
 		<div class="demo-inner-content">
 		<!--/header-w3l-->
-			   <div class="header-w3-agileits" id="home">
-			     <div class="inner-header-agile">	
-								<nav class="navbar navbar-default">
-									<div class="navbar-header">
-										<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-											<span class="sr-only">Toggle navigation</span>
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-										</button>
-										<h1><a  href="index.php?r=site%2Findex2"><span>H</span>otel <p class="s-log">Espejo</p></a>
-										 
-										</h1>
-									</div>
-									<!-- navbar-header -->
-									<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-										
-										<ul class="nav navbar-nav">
-											<li class="active"><a href="index.php?r=site%2Findex2">Inicio</a></li>
-											<li class="dropdown">
-												<a href="" class="dropdown-toggle hvr-bounce-to-bottom" data-hover="Pages" data-toggle="dropdown" aria-expanded="false">Habitaciones <b class="caret"></b></a>
-												 <ul class="dropdown-menu">
-													<li>
-														<a href="index.php?r=site%2Fhabitacion1">Habitación 1</a>
-													</li>
-												</ul>
-											</li>
-											<li><a href="#">Galeria</a></li>
-											<li><a href="#">Contacto</a></li>
-										</ul>
+			<div class="header-w3-agileits" id="home">
+			    <div class="inner-header-agile">	
+					<nav class="navbar navbar-default">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<h1><a  href="index.php?r=site%2Findex2"><span>H</span>otel <p class="s-log">Espejo</p></a>
+							 
+							</h1>
+						</div>
+						<!-- navbar-header -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="index.php?r=site%2Findex2">Inicio</a></li>
+								<li class="dropdown">
+									<a href="" class="dropdown-toggle hvr-bounce-to-bottom" data-hover="Pages" data-toggle="dropdown" aria-expanded="false">Habitaciones <b class="caret"></b></a>
+									 <ul class="dropdown-menu">
+										<li>
+											<a href="index.php?r=site%2Fhabitacion1">Habitación 1</a>
+										</li>
+									</ul>
+								</li>
+								<li><a href="#">Galeria</a></li>
+								<li><a href="#">Contacto</a></li>
+							</ul>
 
-									</div>
-									<div class="clearfix"> </div>	
-								</nav>
-									<div class="w3ls_search">
-													<div class="cd-main-header">
-														<ul class="cd-header-buttons">
-															<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
-														</ul> <!-- cd-header-buttons -->
-													</div>
-													<div id="cd-search" class="cd-search">
-														<form action="#" method="post">
-															<input name="Search" type="search" placeholder="Search...">
-														</form>
-													</div>
-												</div>
-					
-							</div> 
-							<!-- <img src="pagina/images/4.jpg" alt=""> -->
-
+						</div>
+						<div class="clearfix"> </div>	
+					</nav>
+					<div class="w3ls_search">
+						<div class="cd-main-header">
+								<ul class="cd-header-buttons">
+									<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+								</ul> <!-- cd-header-buttons -->
+						</div>
+						<div id="cd-search" class="cd-search">
+							<form action="#" method="post">
+								<input name="Search" type="search" placeholder="Search...">
+							</form>
+						</div>
+					</div>
+				</div> 
 			
 		<!--//header-w3l-->
 			<!--/banner-info-->
 			   <div class="baner-info">   
 			      <h3>Bien<span>venido </span>a   <span>Hotel </span> Espejo</h3>
-				  <h4>Book Your Dream Resort Destinations</h4>
-				  <p>Enjoy Your Stay In</p>
+				  <h4>Disfruta tu estadia!</h4>
+				  <!-- <p>Disfrute su estadia!</p> -->
 			   </div>
 			<!--/banner-ingo-->
 			
@@ -80,7 +77,7 @@ $this->title = 'Hotel Dapa';
  <div class="special featured">
 		  <div class="container">
 				   <div class="ab-w3l-spa">
-				        <h3 class="tittle">Bienvenido al Hotel Espejo</h3>
+				        <h3 class="tittle">Bienvenido a Hotel Espejo</h3>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. ever since the 1500s.Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores hasellusMaecenas ac hendrerit purus. Lorem ipsum dolor sit amet.Lorem Ipsum is simply dummy text of the printing and typesetting industry
 							</p>
 				   </div>
@@ -194,22 +191,22 @@ $this->title = 'Hotel Dapa';
 	<div class="about-bottom">
 		<div class="col-md-6 w3l_about_bottom_left">
 				  <div class="video-grid-single-page-agileits">
-														<div data-video="44fbHx7P-t8" id="video"> <img src="images/watch.jpg" alt="" class="img-responsive" /> </div>
+														<div data-video="44fbHx7P-t8" id="video"> <img src="pagina/images/watch.jpg" alt="" class="img-responsive" /> </div>
 													</div>
 
 			<div class="w3l_about_bottom_left_video">
-				<h4>watch our video</h4>
+				<h4>Nuestro video</h4>
 			</div>
 		</div>
 		<div class="col-md-6 w3l_about_bottom_right one">
-			<h3 class="tittle why">why choose us ?</h3>
+			<h3 class="tittle why">¿Porque elegirnos?</h3>
 			<p>Lorem Aenean aliquet nec mi et lacinia. Aliquam maximus iaculis mi, sed efficitur orci efficitur nec.</p>
 			<div class="panel-group w3l_panel_group_faq" id="accordion" role="tablist" aria-multiselectable="true">
 			  <div class="panel panel-default">
 				<div class="panel-heading" role="tab" id="headingOne">
 				  <h4 class="panel-title asd">
 					<a class="pa_italic" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Restaurant & Banquets
+					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Descripción 1
 					</a>
 				  </h4>
 				</div>
@@ -223,7 +220,7 @@ $this->title = 'Hotel Dapa';
 				<div class="panel-heading" role="tab" id="headingTwo">
 				  <h4 class="panel-title asd">
 					<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Transportation Included
+					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Descripción 2
 					</a>
 				  </h4>
 				</div>
@@ -237,7 +234,7 @@ $this->title = 'Hotel Dapa';
 				<div class="panel-heading" role="tab" id="headingThree">
 				  <h4 class="panel-title asd">
 					<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>The best care for our precious visitors
+					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Descripción 3
 					</a>
 				  </h4>
 				</div>
@@ -252,7 +249,7 @@ $this->title = 'Hotel Dapa';
 				<div class="panel-heading" role="tab" id="headingThree">
 				  <h4 class="panel-title asd">
 					<a class="pa_italic collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Pool Deluxe Room
+					  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><i class="glyphicon glyphicon-minus" aria-hidden="true"></i>Descripción 4
 					</a>
 				  </h4>
 				</div>
@@ -269,13 +266,182 @@ $this->title = 'Hotel Dapa';
 	</div>
 <!-- //about-bottom -->
 
+<!-- about-bottom -->
+	<div class="about-bottom" id="ab">
+	<div class="col-md-6 w3l_about_bottom_right two">
+			<h3 class="tittle why">Reservar</h3>
+			<div class="book-form">
 
+				<form action="#" method="post">
+					<div class="col-md-6 form-date-w3-agileits">
+						<label><i class="fa fa-user" aria-hidden="true"></i> Nombre:</label>
+						<input type="text" name="name" placeholder="Nombre" required="">
+					</div>
+					<div class="col-md-6 form-date-w3-agileits second-agile">
+						<label><i class="fa fa-envelope" aria-hidden="true"></i> Correo:</label>
+						<input type="email" name="email" placeholder="Correo" required="">
+					</div>
+					 <div class="col-md-6 form-date-w3-agileits">
+									<label><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de entrada:</label>
+									<input  id="datepicker" name="Text" type="text" value="mm/dd/yyyy" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="">
+								
+								</div>
+					<!-- <div class="col-md-6 form-time-w3layouts second-agile">
+							<label><i class="fa fa-clock-o" aria-hidden="true"></i> Time :</label>
+							<input type="time">
+					</div> -->
+					<div class="col-md-6 form-date-w3-agileits">
+						        <label><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de salida:</label>
+									<input  id="datepicker1" name="Text" type="text" value="mm/dd/yyyy" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="">
+									
+								</div>
+					<!-- <div class="col-md-6 form-time-w3layouts second-agile">
+							<label><i class="fa fa-clock-o" aria-hidden="true"></i> Time :</label>
+							<input type="time">
+					</div> -->
+					
+					<div class="col-md-6 form-left-agileits-w3layouts second-agile">
+							<label><i class="fa fa-users" aria-hidden="true"></i> Adultos:</label>
+							<select class="form-control">
+								<option></option>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+								<option>Más</option>
+							</select>
+					</div>
 
+					<div class="col-md-6 form-left-agileits-w3layouts second-agile">
+							<label><i class="fa fa-users" aria-hidden="true"></i> Niños:</label>
+							<select class="form-control">
+								<option></option>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+								<option>Más</option>
+							</select>
+					</div>
 
+					<div class="col-md-6 form-left-agileits-w3layouts bottom-w3ls">
+							<label><i class="fa fa-home" aria-hidden="true"></i> Escoge la habiación:</label>
+							<select class="form-control">
+								<option></option>
+								<option>Standard Double Room</option>
+								<option>Standard Family Room</option>
+								<option>Garden Family Room</option>
+								<option>Deluxe Double Room</option>
+								<option>Executive Junior Suite</option>
+								<option>Maisonette</option>
+							</select>
+					</div>
+					<div class="clearfix"> </div>
+					<div class="make wow shake" data-wow-duration="1s" data-wow-delay=".5s">
+						  <input type="submit" value="Reservar">
+					</div>
+				</form>
+			</div>
 
+		</div>
+		<div class="col-md-6 w3l_about_bottom_left">
+			
+<img src="pagina/images/33.jpg" alt="" class="img-responsive" />
+			<div class="w3l_about_bottom_left_video book-text">
+				<h4>Reserva Ya</h4>
+			</div>
+		</div>
+		
+		<div class="clearfix"> </div>
+	</div>
 
+	<br>
 
+<!-- //about -->
 
+<!-- plans -->
+<!-- //plans -->
+      
+<!-- testimonials -->
+<!-- //testimonials -->
+
+ <!-- Footer -->
+	<div class="w3l-footer">
+		<div class="container">
+         <div class="footer-info-agile">
+				<div class="col-md-2 footer-info-grid links">
+					<h4>Hotel Espejo</h4>
+					<ul>
+						       <li><a href="#">Inicio</a></li> 
+								<li><a href="#">Habitaciones</a></li> 
+								<li><a href="#">Galeria</a></li> 
+								<li><a href="#">Contacto</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 footer-info-grid address">
+					<h4>Dirección</h4>
+					<address>
+						<ul>
+							<li>xxxxxxxxxxxxx</li>
+							<li>xxxxxxxxxxxxxxx</li>
+							<li>Cali, Colombia</li>
+							<li>Telephone: 123456789</li>
+							<li>Email: <a class="mail" href="mailto:mail@example.com">info@ejemplo.com</a></li>
+						</ul>
+					</address>
+				</div>
+				<div class="col-md-3 footer-grid">
+				   <h4>habitaciones</h4>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="pagina/images/f1.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="pagina/images/f2.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+						<a href="#"><img src="pagina/images/f3.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="pagina/images/f4.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-4 footer-info-grid">
+				<div class="connect-social">
+					<h4>Conecta con nosotros</h4>
+					<section class="social">
+                        <ul>
+							<li><a class="icon fb" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a class="icon tw" href="#"><i class="fa fa-twitter"></i></a></li>
+							
+							
+							<li><a class="icon pin" href="#"><i class="fa fa-pinterest"></i></a></li>
+							<li><a class="icon db" href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a class="icon gp" href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</section>
+				</div>
+					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<br>
+
+			<!-- <div class="connect-agileits newsletter">
+				<h4>Newsletter</h4>
+					<p>Subscribe to our newsletter and we will inform you about newest projects and promotions.
+					</p>
+					<form action="#" method="post" class="newsletter">
+						<input class="email" type="email" placeholder="Your email address..." required="">
+						<input type="submit" class="submit" value="Subscribe">
+					</form>
+			</div> -->
+	   </div>
+     </div>
+		
+	<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 
 
 
